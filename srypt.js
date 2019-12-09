e3679ca1af0c6b8a7f56690b6d32ebbf3e1fe1c6
WhatIsLove.K <vlad.kovaleko.od@hmail.com>
2019-12-09 20:34:33 +0300

    2 new units

---
+++ srypt.js
@@ -0,0 +1,9 @@
+var croco = new CrocoTurtle(50);
+var chyrka = new MonkeyWithKnife(1);
+var bear = new MonsterBear(2);
+var Boss = new Gyrocopter(99);
+
+croco.attack();
+bear.attack();
+chyrka.attack();
+Boss.attack();
